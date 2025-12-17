c9fb97f1473bfc750fdeb21c04fd4f913aadcec7
Groupix05 <fox.bouche2@gmail.com>
2025-12-17 20:11:05 +0100

    Initial 1.21.1 port

--- src/main/java/com/dtteam/dtaddon_lib/DynamicTreesAddonLib.java
+++ src/main/java/com/dtteam/dtaddon_lib/DynamicTreesAddonLib.java
@@ -1,71 +1,41 @@
 package com.dtteam.dtaddon_lib;
 
-import com.dtteam.dtaddon_lib.init.DTAddonLibPlusRegistries;
-import com.dtteam.dtaddon_lib.init.DTAddonLibRegistries;
-import com.dtteam.dtaddon_lib.resources.RegisterJSONAppliersPlus;
-import com.ferreusveritas.dynamictrees.api.GatherDataHelper;
-import com.ferreusveritas.dynamictrees.api.registry.RegistryHandler;
-import com.ferreusveritas.dynamictrees.block.leaves.LeavesProperties;
-import com.ferreusveritas.dynamictrees.block.rooty.SoilProperties;
-import com.ferreusveritas.dynamictrees.resources.Resources;
-import com.ferreusveritas.dynamictrees.systems.fruit.Fruit;
-import com.ferreusveritas.dynamictrees.systems.pod.Pod;
-import com.ferreusveritas.dynamictrees.tree.family.Family;
-import com.ferreusveritas.dynamictrees.tree.species.Species;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.data.event.GatherDataEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import com.dtteam.dynamictrees.block.fruit.Fruit;
+import com.dtteam.dynamictrees.block.leaves.LeavesProperties;
+import com.dtteam.dynamictrees.block.pod.Pod;
+import com.dtteam.dynamictrees.block.soil.SoilProperties;
+import com.dtteam.dynamictrees.data.GatherDataHelper;
+import com.dtteam.dynamictrees.registry.NeoForgeRegistryHandler;
+import com.dtteam.dynamictrees.tree.family.Family;
+import com.dtteam.dynamictrees.tree.species.Species;
+import com.dtteam.dynamictrees.api.registry.RegistryHandler;
+//import com.dtteam.dynamictreesplus.block.mushroom.CapProperties;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 @Mod(DynamicTreesAddonLib.MOD_ID)
 public final class DynamicTreesAddonLib {
 
     public static final String MOD_ID = "dtaddon_lib";
 
-    public DynamicTreesAddonLib() {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        eventBus.addListener(this::commonSetup);
-        eventBus.addListener(this::clientSetup);
+    public DynamicTreesAddonLib(IEventBus eventBus, ModContainer container) {
         eventBus.addListener(this::gatherData);
 
-        DTAddonLibRegistries.SOUNDS.register(eventBus);
-
-        if (ModList.get().isLoaded("dynamictreesplus")){
-            eventBus.register(DTAddonLibPlusRegistries.class);
-            eventBus.register(RegisterJSONAppliersPlus.class);
-        }
-
-        MinecraftForge.EVENT_BUS.register(this);
-
-        RegistryHandler.setup(MOD_ID);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event) {
-        DTAddonLibRegistries.setup();
-    }
-
-    private void clientSetup(final FMLClientSetupEvent event) {
+        NeoForgeRegistryHandler.setup(MOD_ID, eventBus);
     }
 
     private void gatherData(final GatherDataEvent event) {
-        Resources.MANAGER.gatherData();
-        GatherDataHelper.gatherAllData(
-                MOD_ID, event,
+        GatherDataHelper.gatherAllData(MOD_ID, event,
                 SoilProperties.REGISTRY,
                 Family.REGISTRY,
                 Species.REGISTRY,
                 LeavesProperties.REGISTRY,
                 Fruit.REGISTRY,
                 Pod.REGISTRY
+                //,CapProperties.REGISTRY
         );
     }
 
-    public static ResourceLocation location(final String path) {
-        return new ResourceLocation(MOD_ID, path);
-    }
 }
