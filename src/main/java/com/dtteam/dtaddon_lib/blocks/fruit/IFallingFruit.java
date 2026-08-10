7085deaa532cf1c63465f8586af1a324f332cd67
ninjaguardian <110734253+ninjaguardian@users.noreply.github.com>
2026-08-10 16:21:52 -0500

    Refactor IFallingFruit and fix bonk translation

--- src/main/java/com/dtteam/dtaddon_lib/blocks/fruit/IFallingFruit.java
+++ src/main/java/com/dtteam/dtaddon_lib/blocks/fruit/IFallingFruit.java
@@ -1,6 +1,5 @@
 package com.dtteam.dtaddon_lib.blocks.fruit;
 
-import com.google.common.collect.Lists;
 import com.dtteam.dtaddon_lib.init.DTAddonLibRegistries;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundSource;
@@ -15,10 +14,8 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.block.state.BlockState;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface IFallingFruit {
 
@@ -58,27 +55,33 @@ public interface IFallingFruit {
         return new FallingBlockEntity(world, (double)pos.getX() + 0.5D, pos.getY(), (double)pos.getZ() + 0.5D, state){
 
             @Override
-            public boolean causeFallDamage(float pFallDistance, float pMultiplier, DamageSource pSource) {
+            public boolean causeFallDamage(float pFallDistance, float pMultiplier, @NotNull DamageSource pSource) {
                 int i = (int)Math.ceil(pFallDistance - 1.0F);
-                if (i > 0) {
-                    List<Entity> list = Lists.newArrayList(level().getEntities(this, this.getBoundingBox()));
-                    for(Entity entity : list) {
-                        if (entity instanceof LivingEntity){
-                            entity.hurt(getDamageSource(level()),
-                                    (float)Math.min(Math.floor((float)i * IFallingFruit.fallDamageAmount), IFallingFruit.fallDamageMax) * pMultiplier);
-                            level().playSound(null, pos,
-                                    DTAddonLibRegistries.FRUIT_BONK.get(), SoundSource.BLOCKS,
-                                    1.0F, 1.0F);
-                        }
-                    }
+                if (i <= 0) {
+                    return false;
                 }
+
+                Level level = level();
+
+                for (Entity entity : level.getEntities(this, this.getBoundingBox())) {
+                    if (!(entity instanceof LivingEntity)) {
+                        continue;
+                    }
+
+                    entity.hurt(getDamageSource(level),
+                            (float)Math.min(Math.floor((float)i * IFallingFruit.fallDamageAmount), IFallingFruit.fallDamageMax) * pMultiplier);
+                    level.playSound(null, getX(), getY(), getZ(),
+                            DTAddonLibRegistries.FRUIT_BONK.get(), SoundSource.BLOCKS,
+                            1.0F, 1.0F);
+                }
+
                 return false;
             }
 
             @Nullable
             @Override
-            public ItemEntity spawnAtLocation(@Nonnull ItemStack pStack, float pOffsetY) {
-                return super.spawnAtLocation(getDropOnFallItems( this), pOffsetY);
+            public ItemEntity spawnAtLocation(@NotNull ItemStack pStack, float pOffsetY) {
+                return super.spawnAtLocation(getDropOnFallItems(this), pOffsetY);
             }
         };
     }
